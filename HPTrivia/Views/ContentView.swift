2114b913a8c3815234fb91f2d892efcef0cf61b9
AnaMRodrigues <aninha_s2.acrm@icloud.com>
2026-03-03 14:36:20 -0300

    Added score template; audio active;

--- HPTrivia/Views/ContentView.swift
+++ HPTrivia/Views/ContentView.swift
@@ -53,7 +53,24 @@ struct ContentView: View {
                     
                     Spacer()
                     
-                    Spacer()
+                    VStack {
+                        if animateViewsIn {
+                            VStack {
+                                Text("Recent Scores")
+                                    .font(.title2)
+                                Text("33")
+                                Text("67")
+                                Text("15")
+                            }
+                            .font(.title3)
+                            .foregroundStyle(.white)
+                            .padding(.horizontal)
+                            .background(.black.opacity(0.7))
+                            .clipShape(.rect(cornerRadius: 15))
+                            .transition(.opacity)
+                        }
+                    }
+                    .animation(.linear(duration: 1).delay(4), value: animateViewsIn)
                     
                     Spacer()
                     
@@ -133,12 +150,13 @@ struct ContentView: View {
         .ignoresSafeArea()
         .onAppear {
             animateViewsIn = true
-            //playAudio()
+            playAudio()
         }
         .sheet(isPresented: $showInstructions) {
             Instructions()
         }
     }
+        
     
     private func playAudio() {
         let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
