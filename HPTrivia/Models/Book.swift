7b81220583c0cc91ec19c8b3c510f891e8bb9884
AnaMRodrigues <aninha_s2.acrm@icloud.com>
2026-03-23 11:28:43 -0300

    added the decode function of the questions and organized them for each book;

---
+++ HPTrivia/Models/Book.swift
@@ -0,0 +1,17 @@
+//
+//  Book.swift
+//  HPTrivia
+//
+//  Created by Ana Clara Moreira Rodrigues on 23/03/26.
+//
+
+struct Book: Identifiable {
+    let id: Int
+    let image: String
+    let questions: [Question]
+    let status: BookStatus
+    
+    enum BookStatus {
+        case active, inactive, locked
+    }
+}
