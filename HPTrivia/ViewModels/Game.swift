ba51eeb93e28301a12dee7b7378f98731250a2ca
AnaMRodrigues <aninha_s2.acrm@icloud.com>
2026-03-23 13:42:57 -0300

    Started the selectBooks View; Added a global @Environment property

---
+++ HPTrivia/ViewModels/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  HPTrivia
+//
+//  Created by Ana Clara Moreira Rodrigues on 23/03/26.
+//
+
+import SwiftUI
+
+@Observable
+class Game {
+    var bookQuestions = BookQuestions()
+}
