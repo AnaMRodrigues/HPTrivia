4e25732b1e04273dfe587ac3dfe961e1862ae837
AnaMRodrigues <aninha_s2.acrm@icloud.com>
2026-03-27 14:33:39 -0300

    Added the function to check and to watch for updates on the purchase;

--- HPTrivia/ViewModels/Store.swift
+++ HPTrivia/ViewModels/Store.swift
@@ -15,6 +15,10 @@ class Store {
     
     private var updates: Task<Void, Never>? = nil
     
+    init() {
+        updates = watchForUpdates()
+    }
+    
     // Load available products
     func loadProducts() async {
         do {
@@ -62,6 +66,30 @@ class Store {
     }
     
     // Check for purchased products
+    private func checkPurchased() async {
+        for product in products {
+            guard let status = await product.currentEntitlement else { continue }
+            
+            switch status {
+            case .unverified(let signedType, let verificationError):
+                print("Error on \(signedType): \(verificationError)")
+                
+            case .verified(let signedType):
+                if signedType.revocationDate == nil {
+                    purchased.insert(signedType.productID)
+                } else {
+                    purchased.remove(signedType.productID)
+                }
+            }
+        }
+    }
     
     // Connect with App Store to watch for purchase and transaction updates
+    private func watchForUpdates() -> Task<Void, Never> {
+        Task(priority: .background) {
+            for await _ in Transaction.updates {
+                await checkPurchased()
+            }
+        }
+    }
 }
